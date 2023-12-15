3249106be2d90fb58874dba5158f0ecf6d5cfa9e
newtonjob <jobnewton3@gmail.com>
2023-12-15 10:01:21 +0100

    Improve mobile responsiveness

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="text-right mb-4">
+            <div class="text-right mb-4 mr-5 sm:mr-0">
                 <button type="button"
                         class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                         data-drawer-target="create-user-drawer" data-drawer-show="create-user-drawer"
@@ -18,7 +18,7 @@
             </div>
 
             <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <div class="flex items-center justify-between pb-4 bg-white dark:bg-gray-900 p-5">
+                <div class="flex gap-2 items-center justify-between pb-4 bg-white dark:bg-gray-900 p-5">
                     <div>
                         <button id="dropdownActionButton" data-dropdown-toggle="dropdownAction" class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
                             <span class="sr-only">Filter button</span>
@@ -38,78 +38,79 @@
                         </div>
                     </div>
 
-                    <label for="table-search" class="sr-only">Search</label>
                     <div class="relative">
                         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                             <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                         </div>
-                        <input type="text" id="table-search-users" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search...">
+                        <input type="text" id="table-search-users" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg sm:w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search...">
                     </div>
                 </div>
 
-                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" class="px-6 py-3">
-                            Name
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Category
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Passes
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Action
-                        </th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($invites as $invite)
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                                <img class="w-10 h-10 rounded-full" src="https://ui-avatars.com/api?background=eef6ff&color=3e97ff&name={{ $invite->name }}&format=svg" alt="photo">
-                                <div class="pl-3">
-                                    <div class="text-base font-semibold">{{ $invite->name ?: '-' }}</div>
-                                    <div class="font-normal text-gray-500">{{ $invite->email }}</div>
-                                </div>
+                <div class="overflow-auto">
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Name
                             </th>
-                            <td class="px-6 py-4">
-                                <div class="flex items-center">
-                                    @if ($invite->category == 'VVIP')
-                                        <span class="bg-purple-100 text-purple-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-purple-900 dark:text-purple-300">{{ $invite->category }}</span>
-                                    @else
-                                        <span class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">{{ $invite->category }}</span>
-                                    @endif
-                                </div>
-                            </td>
-                            <td class="px-6 py-4">
-                                {{ $invite->passes }}
-                            </td>
-                            <td class="px-6 py-4">
-                                <div class="flex gap-4">
-                                    <a href="{{ route('invites.show', $invite) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                                        Download Invite
-                                    </a>
-                                    |
-                                    <form action="{{ route('invites.send', $invite) }}" x-data x-submit>
-                                        <button class="font-medium text-green-600 dark:text-green-500 hover:underline">
-                                            {{ $invite->sent() ? 'Resend' : 'Send' }} Invite
-                                        </button>
-                                    </form>
-                                    |
-                                    <form action="{{ route('invites.destroy', $invite) }}" x-data x-submit data-confirm @finish="location.reload()">
-                                        @method('delete')
-                                        <button class="font-medium text-red-600 dark:text-red-500 hover:underline">
-                                            Delete
-                                        </button>
-                                    </form>
-                                </div>
-                            </td>
+                            <th scope="col" class="px-6 py-3">
+                                Category
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Passes
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+
+                            </th>
                         </tr>
-                    </tbody>
-                    @endforeach
-                </table>
+                        </thead>
+                        <tbody>
+                        @foreach($invites as $invite)
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                                    <img class="w-10 h-10 rounded-full" src="https://ui-avatars.com/api?background=eef6ff&color=3e97ff&name={{ $invite->name }}&format=svg" alt="photo">
+                                    <div class="pl-3">
+                                        <div class="text-base font-semibold">{{ $invite->name ?: '-' }}</div>
+                                        <div class="font-normal text-gray-500">{{ $invite->email }}</div>
+                                    </div>
+                                </th>
+                                <td class="px-6 py-4">
+                                    <div class="flex items-center">
+                                        @if ($invite->category == 'VVIP')
+                                            <span class="bg-purple-100 text-purple-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-purple-900 dark:text-purple-300">{{ $invite->category }}</span>
+                                        @else
+                                            <span class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">{{ $invite->category }}</span>
+                                        @endif
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ $invite->passes }}
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="flex gap-4 justify-end">
+                                        <a href="{{ route('invites.show', $invite) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                                            Download Invite
+                                        </a>
+                                        |
+                                        <form action="{{ route('invites.send', $invite) }}" x-data x-submit>
+                                            <button class="font-medium text-green-600 dark:text-green-500 hover:underline">
+                                                {{ $invite->sent() ? 'Resend' : 'Send' }} Invite
+                                            </button>
+                                        </form>
+                                        |
+                                        <form action="{{ route('invites.destroy', $invite) }}" x-data x-submit data-confirm @finish="location.reload()">
+                                            @method('delete')
+                                            <button class="font-medium text-red-600 dark:text-red-500 hover:underline">
+                                                Delete
+                                            </button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                        @endforeach
+                    </table>
+                </div>
             </div>
         </div>
     </div>
