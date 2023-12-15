dfe19af98095784a2ba10679d89667ad88680cad
newtonjob <jobnewton3@gmail.com>
2023-12-15 10:20:32 +0100

    Make email logo rounded

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -2,7 +2,7 @@
 {{-- Header --}}
 <x-slot:header>
 <x-mail::header :url="config('app.url')">
-    <img src="{{ asset('logo.png') }}" class="logo" alt="Logo">
+    <img src="{{ asset('logo.png') }}" class="logo" style="border-radius: 7px" alt="Logo">
 </x-mail::header>
 </x-slot:header>
 
