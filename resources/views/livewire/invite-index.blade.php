fb3cd899373383683c13a5404426c29a43f889e9
newtonjob <jobnewton3@gmail.com>
2023-12-15 10:17:36 +0100

    Formatting

--- resources/views/livewire/invite-index.blade.php
+++ resources/views/livewire/invite-index.blade.php
@@ -10,13 +10,25 @@
             <div id="dropdownAction" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                 <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownActionButton">
                     <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" wire:click="$set('category', '')">Everyone</a>
+                        <a
+                            href="#"
+                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                            wire:click="$set('category', '')"
+                        >Everyone</a>
                     </li>
                     <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" wire:click="$set('category', 'VIP')">VIP</a>
+                        <a
+                            href="#"
+                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                            wire:click="$set('category', 'VIP')"
+                        >VIP</a>
                     </li>
                     <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" wire:click="$set('category', 'VVIP')">VVIP</a>
+                        <a
+                            href="#"
+                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                            wire:click="$set('category', 'VVIP')"
+                        >VVIP</a>
                     </li>
                 </ul>
             </div>
