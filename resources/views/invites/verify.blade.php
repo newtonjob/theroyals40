1e5a2d242ff69236dbb19a01b604eb2fe4ac2d70
newtonjob <jobnewton3@gmail.com>
2024-01-04 21:03:36 +0100

    Langauge

--- resources/views/invites/verify.blade.php
+++ resources/views/invites/verify.blade.php
@@ -39,7 +39,7 @@
                 icon: "info",
                 closeOnClickOutside: false,
                 closeOnEsc: false,
-                buttons: ["Cancel", 'Checkin'],
+                buttons: ["Cancel", 'Pass'],
             }).then(confirm => {
                 if (confirm) location.href = @js(route('invites.checkin', $invite))
             });
