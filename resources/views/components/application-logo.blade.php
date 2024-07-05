94e04a713441275effcff8724944e0bd945d6e3d
newtonjob <jobnewton3@gmail.com>
2024-07-05 18:59:34 +0100

    Customize tenant logos

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('/logo.png') }}" alt="logo" {{ $attributes->merge(['class' => 'rounded-xl']) }} width="{{ $width ?? 50 }}">
+<img src="{{ tenant()->icon }}" alt="logo" {{ $attributes->merge(['class' => 'rounded-xl']) }} width="{{ $width ?? 50 }}">
