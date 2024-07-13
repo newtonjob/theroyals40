c994417b3c751f2a57beec2bb3772d1285fa6577
newtonjob <jobnewton3@gmail.com>
2024-07-13 16:22:17 +0100

    No default value

--- resources/views/partials/create-invite-modal.blade.php
+++ resources/views/partials/create-invite-modal.blade.php
@@ -56,7 +56,7 @@
                 name="passes" type="number"
                 class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="How many passes?" aria-label="passes" required
-                value="1" min="1"
+                min="1"
             >
         </div>
 
