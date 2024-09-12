31680f02c2a5367f442c37a91c5ea8eb5fb4a11a
newtonjob <jobnewton3@gmail.com>
2024-09-12 22:07:06 +0100

    Register a standalone tenancy service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,13 +2,7 @@
 
 namespace App\Providers;
 
-use App\Listeners\ConfigureTenant;
-use App\Models\Tenant;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Queue\Events\JobProcessing;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Context;
-use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\ServiceProvider;
 use Livewire\Livewire;
 
@@ -19,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->singleton(ConfigureTenant::class);
+        //
     }
 
     /**
@@ -30,32 +24,5 @@ class AppServiceProvider extends ServiceProvider
         Livewire::forceAssetInjection();
 
         Model::unguard();
-
-        $this->configureRequests();
-        $this->configureQueues();
-    }
-
-    /**
-     * Configure queues to run within the context of the tenant that dispatched the job.
-     */
-    public function configureQueues(): void
-    {
-        Queue::before(function (JobProcessing $event) {
-            $id = Context::get('tenantId');
-            $id ? Tenant::findOrFail($id)->use() : Tenant::current()?->forget();
-        });
-    }
-
-    /**
-     * Configure requests to run within the context of the tenant.
-     */
-    public function configureRequests(): void
-    {
-        if (! $this->app->runningInConsole()) {
-            Cache::remember(
-                $domain = request()->host(), now()->addHour(),
-                fn () => Tenant::whereDomain($domain)->first()
-            )?->use();
-        }
     }
 }
--- config/app.php
+++ config/app.php
@@ -171,6 +171,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\PdfServiceProvider::class,
+        App\Providers\TenancyServiceProvider::class,
     ])->toArray(),
 
     /*
