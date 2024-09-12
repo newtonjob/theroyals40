12095021246a9f1e03e760bdcdb9f6623b510171
newtonjob <jobnewton3@gmail.com>
2024-09-12 21:02:06 +0100

    make `tenant_id` nullable in `users` table

--- database/migrations/2018_10_19_000000_create_users_table.php
+++ database/migrations/2018_10_19_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('tenant_id')->constrained();
+            $table->foreignId('tenant_id')->nullable()->constrained();
 
             $table->string('name');
             $table->string('email');
