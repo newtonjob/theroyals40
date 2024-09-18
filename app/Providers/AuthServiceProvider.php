72626fee646f5111f4ebc0f0129bf3e8ee348ac4
newtonjob <jobnewton3@gmail.com>
2024-09-18 18:59:42 +0100

    Clean up

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -10,15 +10,6 @@ use Illuminate\Support\Facades\Gate;
 class AuthServiceProvider extends ServiceProvider
 {
     /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        //
-    ];
-
-    /**
      * Register any authentication / authorization services.
      */
     public function boot(): void
