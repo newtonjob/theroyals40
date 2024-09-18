35953f6ec6d953eb09bc2d6ab608f27e19cca70d
newtonjob <jobnewton3@gmail.com>
2024-09-18 18:22:56 +0100

    wip

--- app/Models/Invite.php
+++ app/Models/Invite.php
@@ -78,6 +78,6 @@ class Invite extends Model implements Attachable
             url()->signedRoute('invites.show', $this)
         ]);
 
-        return "whatsapp://send?text=".$text;
+        return "whatsapp://send?text=".urlencode($text);
     }
 }
